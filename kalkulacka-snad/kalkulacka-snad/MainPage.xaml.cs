e36d517ce2ff3b2ac42ee705bdeef8668d52e12a
bendado17 <bendado17@sps-prosek.cz>
2020-10-11 15:54:26 +0200

    Přidejte soubory projektu.

---
+++ kalkulacka-snad/kalkulacka-snad/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Z.Expressions;
+
+namespace kalkulacka_snad
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public string Count(string expression)
+        {
+            string result = "";
+            try
+            {
+                var outcome = Eval.Execute<int>(expression, 1, 2);
+
+                result = outcome.ToString();
+
+            }
+            catch
+            {
+                result = "ERROR";
+            }
+            return result;
+        }
+
+        void onButtonClicked(object sender, EventArgs args)
+        {
+
+            // Rozpozna tlacitko
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+
+            // Akce pro ruzna tlacitka
+            if (buttonText == "=")
+            {
+                LabelCal.Text = Count(LabelCal.Text);
+            }
+            if (buttonText == "DEL")
+            {
+                string text = LabelCal.Text;
+                if (text == "ERROR")
+                {
+                    LabelCal.Text = "";
+                }
+                else
+                {
+                    LabelCal.Text = text.Substring(0, text.Length - 1);
+                }
+            }
+            if (buttonText == "C")
+            {
+                LabelCal.Text = "";
+            }
+            if (buttonText != "=" || buttonText != "DEL" || buttonText != "C")
+            {
+                if (LabelCal.Text == "ERROR")
+                {
+                    LabelCal.Text = buttonText;
+                }
+                else
+                {
+                    LabelCal.Text += buttonText;
+                }
+            }
+        }
+    }
+}
